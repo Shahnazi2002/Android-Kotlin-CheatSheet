264cce3ba8095dd02d197970af0a06216f1ca765
Mohammad SN <shahnazi2002@gmail.com>
2022-10-24 19:53:15 +0330

    Create ViewBinding.kt

---
+++ ViewBinding.kt
@@ -0,0 +1,37 @@
+// Enabling View Binding
+// In module-level build.gradle file, add the following code to enable view binding:
+
+android {
+    ...
+    buildFeatures {
+        viewBinding true
+    }
+}
+
+
+// Using View Binding in Activities
+
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var binding: ActivityMainBinding
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        
+        ...
+    }
+}
+
+
+// Using View Binding in Dialogs
+
+fun showDialog() {
+    val dialog: DialogBinding = DialogBinding.inflate(LayoutInflater.from(this))
+    val alertDialog = AlertDialog.Builder(this).setView(dialog.root).create()
+    
+    ...
+    
+    alertDialog.show()
+}
