7bddbb9038532371b3c34e2b24b4772ad4fcc4f7
Mohammad SN <shahnazi2002@gmail.com>
2023-02-17 12:31:40 +0330

    Update ForceRTL.kt

--- ForceRTL.kt
+++ ForceRTL.kt
@@ -1,7 +1,20 @@
 // https://stackoverflow.com/a/74772405/18139991
 
-fun forceRTLIfSupported() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-        window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+package ir.example.persianapp
+
+import ...
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        forceRTLIfSupported()
+        supportActionBar?.title = "عنوان"
+    }
+
+    fun forceRTLIfSupported() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            window.decorView.layoutDirection = View.LAYOUT_DIRECTION_RTL
+        }
     }
 }
