ec27a2e4c8706d098ed797b4abffa63688420486
Mohammad SN <shahnazi2002@gmail.com>
2023-05-17 20:17:57 +0330

    Reform NotesData.kt

--- RoomDatabase/NotesData.kt
+++ RoomDatabase/NotesData.kt
@@ -1,13 +1,12 @@
-package ir.mohammadsn.wcounter.database
+package ir.mohammadsn.myapp.database
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "appdata")
-data class WorkoutData(
+data class NotesData(
     @PrimaryKey val id: Int,
     @ColumnInfo(name = "title") val title: String,
-    @ColumnInfo(name = "date") val date: String,
-    @ColumnInfo(name = "count") val count: Int
+    @ColumnInfo(name = "description") val description: String
     )
